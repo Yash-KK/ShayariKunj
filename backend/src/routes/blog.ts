72bd07c0120d7ccd12e15a40b9b273f6e505bdab
Yash-KK <reachyashk@gmail.com>
2025-01-16 19:23:39 +0530

    UD-blogs

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -80,4 +80,83 @@ blogRouter.post("/", async (c) => {
     );
   }
 });
+
+blogRouter.get("/:id", async (c) => {
+  const prisma = getPrisma(c.env.DATABASE_URL);
+  const blogId = c.req.param("id");
+  const blog = await prisma.blog.findUnique({
+    where: {
+      id: blogId,
+    },
+  });
+  if (!blog) {
+    return c.json({
+      status: "fail",
+      message: `blog with mentioned Id does not exist`,
+    });
+  }
+  return c.json(
+    {
+      blog: blog,
+    },
+    200,
+  );
+});
+
+blogRouter.patch("/:id", async (c) => {
+  const prisma = getPrisma(c.env.DATABASE_URL);
+  const blogId = c.req.param("id");
+  const body = await c.req.json();
+  try {
+    const blog = await prisma.blog.update({
+      where: {
+        id: blogId,
+      },
+      data: body,
+    });
+    return c.json(
+      {
+        status: "success",
+        blog: blog,
+      },
+      200,
+    );
+  } catch (error) {
+    return c.json(
+      {
+        status: "fail",
+        message: "could not update blog",
+      },
+      400,
+    );
+  }
+});
+
+blogRouter.delete("/:id", async (c) => {
+  const prisma = getPrisma(c.env.DATABASE_URL);
+  const blogId = c.req.param("id");
+  try {
+    const blog = await prisma.blog.delete({
+      where: {
+        id: blogId,
+      },
+    });
+    return c.json(
+      {
+        status: "success",
+        blog: blog,
+      },
+      200,
+    );
+  } catch (error) {
+    return c.json(
+      {
+        status: "fail",
+        message: "could not delete blog",
+      },
+      400,
+    );
+  }
+});
+
 export default blogRouter;
