db744281d652ff43b45dfb1dbf8cb946c93e5744
Yash-KK <reachyashk@gmail.com>
2025-01-18 23:28:14 +0530

    add-quote-page

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,16 @@
+export default function Quote() {
+  return (
+    <div className="flex justify-center items-center h-svh bg-slate-200">
+      <div>
+        <div className="px-32 font-mono font-bold text-3xl">
+          "Suno, ek rai hai, ishq se behetar ek cup chai hai. Ishq mein jo milta
+          hai, wo kabhi kabhi adhura lagta hai, par chai hamesha puri hoti
+          hai.""
+        </div>
+        <div className="px-32 mt-4 font-medium font-mono hover:font-serif">
+          Ghar Ke Samne Ka Chaiwala
+        </div>
+      </div>
+    </div>
+  );
+}
