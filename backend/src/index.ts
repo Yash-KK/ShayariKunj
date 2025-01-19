fd1b1e48bf9711b40bca35d4f5f155e987164d62
Yash-KK <reachyashk@gmail.com>
2025-01-19 18:00:54 +0530

    create and get tags

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,12 @@
 import { Hono } from "hono";
 import authorRouter from "./routes/author";
 import quoteRouter from "./routes/quote";
+import tagRouter from "./routes/tag";
 
 const app = new Hono();
 app.route("/api/v1/author", authorRouter);
 app.route("/api/v1/quotes", quoteRouter);
-
+app.route("/api/v1/tags", tagRouter);
 app.get("/", async (c) => {
   return c.text("Hello Hono!");
 });
