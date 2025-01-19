05ab11a049b62f6f775ddca54a6db25e34b6f16e
Yash-KK <reachyashk@gmail.com>
2025-01-19 16:10:23 +0530

    add quote middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
 import authorRouter from "./routes/author";
+import quoteRouter from "./routes/quote";
 
 const app = new Hono();
 app.route("/api/v1/author", authorRouter);
+app.route("/api/v1/quotes", quoteRouter);
 
 app.get("/", async (c) => {
   return c.text("Hello Hono!");
