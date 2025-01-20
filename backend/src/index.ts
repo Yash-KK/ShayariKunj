6300de6e0c7709042c4b1928bdbe625d1d8ddbe9
Yash-KK <reachyashk@gmail.com>
2025-01-21 01:54:56 +0530

    fetch all to-be-approved quotes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,13 @@ import { Hono } from "hono";
 import authorRouter from "./routes/author";
 import quoteRouter from "./routes/quote";
 import tagRouter from "./routes/tag";
+import adminRouter from "./routes/admin";
 
 const app = new Hono();
 app.route("/api/v1/author", authorRouter);
 app.route("/api/v1/quotes", quoteRouter);
 app.route("/api/v1/tags", tagRouter);
+app.route("/api/v1/admin", adminRouter);
 app.get("/", async (c) => {
   return c.text("Hello Hono!");
 });
